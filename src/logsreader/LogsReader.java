924607a1244c3d60a4bebcd9d8882a5da033c19a
Przemo <Przemo@192.168.2.2>
2013-08-14 22:34:23 +0100

    LogEntry finished.
    readData function returning Queue of LogEntries from a given file finished and tested.

--- src/logsreader/LogsReader.java
+++ src/logsreader/LogsReader.java
@@ -7,6 +7,8 @@ package logsreader;
 import gui.MainWindow;
 import gubas.javaapplication1.FormsCaller;
 import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -28,8 +30,8 @@ public class LogsReader {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Queue d = readData("E:/test", 50);
-        //FormsCaller.callNewWindow("LogReader", new MainWindow());
+            Queue d = readData(LogsReader.class.getResource("../tests/test.txt").getFile(), 23);
+            //FormsCaller.callNewWindow("LogReader", new MainWindow());
     }
     
     public static Queue readData(String fileName, int capacity){
@@ -38,27 +40,35 @@ public class LogsReader {
         try {
             BufferedReader reader = new BufferedReader(new FileReader(f));
             //List<String> entry = new ArrayList<>();
+            StringBuilder restOfEntry = new StringBuilder();
+            LogEntry entry = null;
                 while(reader.ready()){
-                    LogEntry entry;
+                    
                     if(ret.size()==capacity){
-                        ret.remove();
+                        ret.poll();
+                        restOfEntry = new StringBuilder();
+                        entry = null;
                     }
                     String l = reader.readLine();
-                    String restOfEntry;
-                    if(l.contains("[") && l.contains("]")){
-                        l=l.replaceAll("\\]", "");
-                        entry = new LogEntry(new String[]{});
-                        //entry.addAll(Arrays.asList(l.split("\\[")));
+                    
+                    if(l.contains(LogEntry.logEntryElementStartMarker) && l.contains(LogEntry.logEntryElementEndMarker)){ //we assume that the log file has got these signs inside and that the rest does not
+                        if(restOfEntry.length()>0 && entry !=null){
+                            entry.setDescription(restOfEntry.toString());
+                        }
+                        restOfEntry = new StringBuilder();
+                        l=l.replaceAll(LogEntry.logEntryElementStartMarkerRegExp, "");
+                        entry = new LogEntry(l.split(LogEntry.logEntryElementEndMarkerRegExp));
+                        ret.offer(entry);
                         
                     } else {
-                        
-                    }
-                    
+                        if(entry != null){
+                          restOfEntry.append(l);  
+                        }                     
+                    }    
                 }
         } catch (IOException ex) {
             Logger.getLogger(LogsReader.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
         return ret;
     }
 }
--- src/misc/LogEntry.java
+++ src/misc/LogEntry.java
@@ -4,6 +4,8 @@
  */
 package misc;
 
+import java.text.DateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,6 +15,13 @@ import java.util.Map;
  */
 public class LogEntry {
     
+    public static String logEntryElementStartMarker = "[";
+    
+    public static String logEntryElementEndMarker = "]";
+    
+    public static String logEntryElementStartMarkerRegExp = "\\[";
+    
+    public static String logEntryElementEndMarkerRegExp = "\\]";
     
     public static String[] Columns = new String[]{"Time from start", "Date", "Thread name", "Class Name", "Source", "Level", "Message"};
     
@@ -27,21 +36,42 @@ public class LogEntry {
                                                                  put(5,5);
                                                                  put(6,6);}};
     
-    protected String time, date, thread, className, sourceName, level, message;
+    protected String time, date, thread, className, sourceName, level, message, description;
 
-    public void setMessage(String message) {
+    public void setDescription(String message) {
         if(message!=null){
-           this.message = message; 
+           this.description = message; 
         }
     }
     
      public LogEntry(String[] head){
-         time = head[mapping.get(0)];
-         date = head[mapping.get(1)];
-         thread = head[mapping.get(2)];
-         className = head[mapping.get(3)];
-         sourceName = head[mapping.get(4)];
-         level=head[mapping.get(5)];
+         if(head.length>0){
+             try{
+           Integer.parseInt(head[mapping.get(0)]);
+           time = head[mapping.get(0)];  
+             } catch(NumberFormatException ex){
+                 time = null;
+                 return;
+             }
+         }
+         if(head.length>1){
+           date = head[mapping.get(1)];        
+         }
+         if(head.length>2){
+          thread = head[mapping.get(2)];   
+         }
+         if(head.length>3){
+          className = head[mapping.get(3)];   
+         }
+         if(head.length>4){
+          sourceName = head[mapping.get(4)];   
+         }
+         if(head.length>5){
+           level=head[mapping.get(5)];  
+         }
+         if(head.length>6){
+             message=head[mapping.get(6)];
+         }
      }
 
      
